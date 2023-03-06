2148a52f12f0c3b119b02ce6d1028b1c19f7ad3a
Brandon <bbrungardt02@gmail.com>
2023-03-06 10:16:22 -0600

    Last commit

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Your code here
+let chosenColor = "red";
 const table = document.getElementsByTagName("table")[0];
 
 function makeRow() {
@@ -15,17 +16,19 @@ makeRow();
 let addRow = document.getElementById("add-row");
 addRow.addEventListener("click", makeRow);
 
-
-
-table.addEventListener('click',colorize);
-function colorize (event){
+table.addEventListener("click", colorize);
+function colorize(event) {
   const target = event.target;
-  if (target.className === '')
-  {
-    target.className = 'red';
+  if (event.target.tagName === "TD") {
+    if (target.className === chosenColor) {
+      target.className = "";
+    } else {
+      target.className = chosenColor;
+    }
   }
-  else{
-    target.className = '';
-  }
-  
 }
+
+let colorSelector = document.querySelector("select");
+colorSelector.addEventListener("change", (event) => {
+  chosenColor = event.target.value;
+});
